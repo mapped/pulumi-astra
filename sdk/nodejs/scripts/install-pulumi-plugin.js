186ba5611448030649733538e2fa743a409f1702
Ivan Sabelnikov <ivan@mapped.com>
2022-06-06 17:26:48 +0300

    Fix broken download url

--- sdk/nodejs/scripts/install-pulumi-plugin.js
+++ sdk/nodejs/scripts/install-pulumi-plugin.js
@@ -7,7 +7,7 @@ if (args.indexOf("${VERSION}") !== -1) {
 	process.exit(0);
 }
 
-var res = childProcess.spawnSync("pulumi", ["plugin", "install", "--server", "https://github.com/pulumiverse/pulumi-astra/releases/download/${VERSION}"].concat(args), {
+var res = childProcess.spawnSync("pulumi", ["plugin", "install", "--server", "https://github.com/pulumiverse/pulumi-astra/releases/download/v${VERSION}"].concat(args), {
     stdio: ["ignore", "inherit", "inherit"]
 });
 
